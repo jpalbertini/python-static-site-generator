08cfe9fac8250859f52ce994ddfa04bbe87f5677
winux <jpalbertini@live.fr>
2020-04-30 22:19:22 +0200

    first module

--- ssg/site.py
+++ ssg/site.py
@@ -2,9 +2,15 @@ from pathlib import Path
 
 class Site:
     def __init__(self, source, dest):
-        self.src = Path(source)
-        self.dst = Path(dest)
+        self.source = Path(source)
+        self.dest = Path(dest)
 
-    def createDir(self, path):
-        directory = self.dst / path.relative_to()
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
+
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
